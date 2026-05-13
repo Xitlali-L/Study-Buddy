4f2cffc8daa2a25fc360bc7568ba411049e1ce67
Xitlali-L <167123572+Xitlali-L@users.noreply.github.com>
2026-05-12 22:27:46 -0500

    corrected the timer to allow user input with a settings menu. Also included icons to click settings and to go back to character customizer. Added a Favicon as well.

--- public/scripts/animation.js
+++ public/scripts/animation.js
@@ -11,7 +11,8 @@ const charAnim = [
 
 
 let timer = false;
-let timeLeft = 300;
+let timeLeft = 1800;  //30 minutes
+let duration = 1800;
 let currentFrame = 0;
 let position = 0;
 let frameCount = 0;
@@ -20,11 +21,58 @@ let timerLoop = null;
 let xPos = 50;
 let yPos = window.innerHeight - 150;
 
+
 const content = document.getElementById("content");
 const actBtn = document.getElementById("actBtn");
-const imgEle = document.getElementById("charFrame");
 
-display();
+const imgEle = document.getElementById("charFrame");
+const setIcon = document.getElementById("setIcon");
+const setCng = document.getElementById("setChange");
+const setMenu = document.getElementById("setMenu");
+
+const timeInput = document.getElementById("timeInput");
+
+const finBtn = document.getElementById("fin");
+const defaultBtn = document.getElementById("def");
+
+//if timer is not running then menu can appear or disappear based on click
+setIcon.onclick = function(){
+   if(!timer){
+    setMenu.classList.toggle("hidden");
+   }
+};
+
+setCng.onclick = function(){
+    window.location.href = "sketch.html";
+}
+
+//enters users input from timer into the timer countdown after clicking done
+finBtn.onclick = function(){
+    let writeMin = parseInt(timeInput.value, 10);
+
+    if(isNaN(writeMin) || writeMin <= 0){
+        timeLeft = 1800;
+        duration = 1800;
+        timeInput.value = 30;
+    }
+    else{
+        timeLeft = writeMin * 60;
+        duration = writeMin * 60;
+    }
+
+    display()
+    setMenu.classList.add("hidden");
+};
+
+//if user clicks default button timer is set to 30 mins
+defaultBtn.onclick = function(){
+    timeLeft = 1800;
+    duration = 1800;
+    timeInput.value = 30;
+    display();
+    setMenu.classList.add("hidden");
+};
+
 
 //timer display
 function display(){
@@ -37,6 +85,8 @@ content.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
 
 //initalize character position
 imgEle.style.transform = `translate(${xPos}px, ${yPos}px)`;
+//display timer
+display();
 
 function gameMovement(){
     if(!timer){
@@ -68,6 +118,7 @@ function gameMovement(){
 
 actBtn.onclick = function(){
     if(!timer){
+        setMenu.classList.add("hidden");
         timer = true;
         actBtn.textContent = "Stop";
         gameMovement();
@@ -85,14 +136,24 @@ actBtn.onclick = function(){
         }, 1000);
     }
     else{
-        stop();
+        pause();
     }
 };
+
+function pause(){
+    timer = false;
+    clearInterval(timerLoop);
+    timerLoop = null;
+    actBtn.textContent = "Start Studying";
+}
 
 function stop(){
     timer = false;
     clearInterval(timerLoop);
     timerLoop = null;
     actBtn.textContent = "Start Studying";
+
+    timeLeft = duration;
+    display();
 }
 
